630bfb425ae15f411a83c2e9e2f93023cef95c5e
XStephBarreto <contact.stephaniebarreto@gmail.com>
2024-11-02 23:10:50 -0400

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject enemy2;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, transform.position, Quaternion.identity);
+        InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemy2", 10f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateEnemy2()
+    {
+        Instantiate(enemy2, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
+    }
+}
